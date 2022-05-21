c4f17b719b805389c3feaeb7a9727fa420383f0f
linyys <1398809407@qq.com>
2022-05-21 14:24:07 +0800

    fix button

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,26 +1,28 @@
 <template>
   <button
-    ref="_ref"
-    class="l-button"
-    :style="[buttonSizeStyle, buttonRoundStyle]"
-    :class="[`l-button-${props.size}`]"
-    @click="animation()"
+    ref='_ref'
+    class='l-button'
+    :style='[buttonSizeStyle, buttonRoundStyle]'
+    :class='[`l-button-${props.size}`]'
+    @click='animation()'
   >
     <slot></slot>
   </button>
 </template>
-<script lang="ts" setup>
-import { ref } from 'vue'
+<script lang='ts' setup>
+import { onBeforeMount, ref } from 'vue'
 import { buttonProps } from './button'
 
 const _ref = ref<HTMLButtonElement>()
+let interval
 // 控制按钮动画
 const animation = () => {
   if (props.type == 'text') {
     return
   }
+
   let time = 1
-  const interval = setInterval(() => {
+  interval = setInterval(() => {
     time++
     _ref.value!.style.boxShadow = `0 0 0 ${time}px  ${props.color}${6 - time}5`
     if (time === 6) {
@@ -29,38 +31,43 @@ const animation = () => {
     }
   }, 60)
 }
+onBeforeMount(() => {
+  clearInterval(interval)
+})
 const props = defineProps(buttonProps)
 // 大小
 const buttonSizeStyle =
   props.type === 'hollow'
     ? {
-        color: props.color,
-        border: `1px solid ${props.color}`,
-        backgroundColor: 'rgba(0,0,0,0)'
-      }
+      color: props.color,
+      border: `1px solid ${props.color}`,
+      backgroundColor: 'rgba(0,0,0,0)'
+    }
     : props.type === 'text'
-    ? {
+      ? {
         backgroundColor: 'rgba(0,0,0,0)',
         border: '0'
       }
-    : props.type === 'secondary'
-    ? {
-        backgroundColor: props.color + 25,
-        border: `1px solid ${props.color}25`,
-        color: props.color
-      }
-    : {
-        backgroundColor: props.color,
-        border: `1px solid ${props.color}`
-      }
+      : props.type === 'secondary'
+        ? {
+          backgroundColor: props.color + 25,
+          border: `1px solid ${props.color}25`,
+          color: props.color
+        }
+        : {
+          backgroundColor: props.color,
+          border: `1px solid ${props.color}`
+        }
 // 圆角
 const buttonRoundStyle = props.round
   ? { borderRadius: '12px' }
   : { borderRadius: '4px' }
 </script>
-<script lang="ts">
+<script lang='ts'>
 export default {
   name: 'LButton'
 }
 </script>
-<style scoped></style>
+<style scoped>
+
+</style>
